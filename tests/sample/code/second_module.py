10ad7c88b2ffd5085934fa53641aa6335d1bd2e0
pksol <kogan.peter@gmail.com>
2019-11-24 17:24:19 +0200

    support sending a class for mock use cases
    Also some formatting issues.

--- tests/sample/code/second_module.py
+++ tests/sample/code/second_module.py
@@ -1,2 +1,10 @@
 def my_dir(obj):
     return dir(obj)
+
+
+class SingleClassInModule(object):
+    def __init__(self, prop):
+        raise NotImplementedError()
+
+    def not_implemented(self, param):
+        raise NotImplementedError()
